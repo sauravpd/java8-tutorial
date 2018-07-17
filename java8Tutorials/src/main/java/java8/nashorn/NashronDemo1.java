ec690e48d44de7f22439c764c0bed77fa231acf0
saurav <sauravpd58@gmail.com>
2018-07-17 23:56:06 +0530

    added nashorn sample cde

---
+++ java8Tutorials/src/main/java/java8/nashorn/NashronDemo1.java
@@ -0,0 +1,20 @@
+package java8.nashorn;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
+/**
+ * 
+ * @author saurav
+ *
+ */
+public class NashronDemo1 
+{
+	public static void main(String[] args) throws ScriptException 
+	{
+		ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+    
+    
+	}
+}
