67671721e7d06390dc9b3a253a9a8ad23571e5d5
saurav <sauravpd58@gmail.com>
2018-07-18 23:50:22 +0530

    added lambda code sample

---
+++ java8Tutorials/src/main/java/java8/lambda/LamdaDemo2.java
@@ -0,0 +1,19 @@
+package java8.lambda;
+
+interface Calculator
+{  
+	int add(int a,int b);  
+}  
+/**
+ * 
+ * @author saurav
+ *
+ */
+public class LamdaDemo2 
+{
+	public static void main(String[] args) 
+	{
+		Calculator calc=(a,b)->(a+b);
+		System.out.println(calc.add(10,10)); // output : 20
+	}
+}
