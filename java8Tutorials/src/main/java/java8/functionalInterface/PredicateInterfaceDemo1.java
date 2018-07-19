59c8b08cf993e8cb2f8e3dc929bba400d9897569
saurav <sauravpd58@gmail.com>
2018-07-19 23:39:16 +0530

    added Predicate sample code

---
+++ java8Tutorials/src/main/java/java8/functionalInterface/PredicateInterfaceDemo1.java
@@ -0,0 +1,28 @@
+package java8.functionalInterface;
+
+import java.util.function.Predicate;
+
+/**
+ * 
+ * @author saurav
+ *
+ */
+public class PredicateInterfaceDemo1 
+{
+	public static Boolean checkSpeed(int speed)
+	{  
+		if(speed>20)
+		{
+			return true;
+		}
+		else
+		{
+			return false;  
+		}
+	}  
+	public static void main(String[] args) 
+	{
+		Predicate<Integer> predicate=PredicateInterfaceDemo1::checkSpeed;
+		System.out.println(predicate.test(50)); // output : true
+	}
+}
