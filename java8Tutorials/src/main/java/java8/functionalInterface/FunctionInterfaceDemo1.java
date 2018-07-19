7456de488f1920679b6d6993ce5282603295c96e
saurav <sauravpd58@gmail.com>
2018-07-20 00:09:43 +0530

    added Function sample code

---
+++ java8Tutorials/src/main/java/java8/functionalInterface/FunctionInterfaceDemo1.java
@@ -0,0 +1,21 @@
+package java8.functionalInterface;
+
+import java.util.function.Function;
+/**
+ * 
+ * @author saurav
+ *
+ */
+public class FunctionInterfaceDemo1 
+{
+	public static Integer show(String message){  
+		return Integer.parseInt(message);  
+	}  
+	public static void main(String[] args) 
+	{  
+		// Function interface referring to a method  
+		Function<String, Integer> fun = FunctionInterfaceDemo1::show;  
+		// Calling Function interface method  
+		System.out.println(fun.apply("4545"));  // output : 4545
+	}  
+}
