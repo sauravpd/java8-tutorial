d96a85204f2d95a21d17d57c0462d43d56176ab5
saurav <sauravpd58@gmail.com>
2018-07-19 23:32:24 +0530

    added  Method References sample code

---
+++ java8Tutorials/src/main/java/java8/lambda/LambdaDemo5.java
@@ -0,0 +1,18 @@
+package java8.lambda;
+interface Calc
+{
+	void add(int a,int b);
+}
+public class LambdaDemo5 
+{
+	public LambdaDemo5(int x,int y)
+	{
+		System.out.println(x+y);
+	}
+	public static void main(String[] args) 
+	{
+		// Reference to Constructor.
+		Calc calc=LambdaDemo5::new;
+		calc.add(10,10); // output : 20
+	}
+}
