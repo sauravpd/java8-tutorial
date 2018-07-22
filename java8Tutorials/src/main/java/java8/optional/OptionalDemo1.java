d68631475e948a8db2c8e28d489b4007c03e69d8
saurav <sauravpd58@gmail.com>
2018-07-23 00:21:13 +0530

    added optional sample code

---
+++ java8Tutorials/src/main/java/java8/optional/OptionalDemo1.java
@@ -0,0 +1,22 @@
+package java8.optional;
+
+import java.util.Optional;
+import java.util.function.Consumer;
+
+public class OptionalDemo1 
+{
+	public static void main(String[] args) 
+	{
+		Optional<String> city=Optional.of("Pune");
+		System.out.println(city);
+
+		Optional<String> emptyGender = Optional.empty();
+
+		if (city.isPresent()) {
+			System.out.println("Value present.");
+		} else {
+			System.out.println("Value not present.");
+		}
+		city.ifPresent(a->System.out.println("value present"));
+	}
+}
