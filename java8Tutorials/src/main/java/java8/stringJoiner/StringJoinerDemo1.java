2f0a5090119a3a1d98ddf773762a44288ab3f774
saurav <sauravpd58@gmail.com>
2018-07-22 23:42:24 +0530

    added stringjoiner sample code

---
+++ java8Tutorials/src/main/java/java8/stringJoiner/StringJoinerDemo1.java
@@ -0,0 +1,16 @@
+package java8.stringJoiner;
+
+import java.util.StringJoiner;
+
+public class StringJoinerDemo1
+{
+	public static void main(String[] args) 
+	{
+		StringJoiner joiner = new StringJoiner(",", "[", "]");
+		joiner.add("Pune")
+		.add("Mumbai")
+		.add("Delhi")
+		.add("Banglore");
+		System.out.println(joiner); // output : [Pune,Mumbai,Delhi,Banglore]		
+	}
+}
